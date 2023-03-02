560f8972da212899f9b8298a11ee1e629a8d5aae
Chiagbaizu Charles <charleschinagorom10.com>
2023-03-02 07:16:04 +0100

    first-commit

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
+const menu = document.querySelector(".rebank-menu");
+
 const faqs = document.querySelectorAll(".faq");
 
-// faqs.forEach((faq) => {
-//   const btns = faq.querySelectorAll("svg");
-//   btns.forEach((btn) => {
-//     btn.addEventListener("click", () => {
-//       faqs.forEach((content) => {
-//         if (content !== faq) {
-//           content.classList.remove("active");
-//         }
-//       });
-//       faq.classList.toggle("active");
-//     });
-//   });
-//   });
-
 faqs.forEach((faq) => {
-  const questions = faq.querySelectorAll(".question");
-  faq.addEventListener("click", () => {
-    questions.forEach((question) => {
-      if (question !== question) {
-        faq.classList.remove("active");
-      }
+  const btns = faq.querySelectorAll("svg");
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      faqs.forEach((question) => {
+        if (question !== faq) {
+          question.classList.remove("active");
+        }
+      });
       faq.classList.toggle("active");
     });
   });
 });
+
+// faqs.forEach((faq) => {
+//   const questions = faq.querySelectorAll(".question");
+//   faq.addEventListener("click", () => {
+//     questions.forEach((question) => {
+//       if (question !== question) {
+//         faq.classList.remove("active");
+//       }
+//       faq.classList.toggle("active");
+//     });
+//   });
+// });
 
 const year = document.getElementById("year");
 const d = new Date();
