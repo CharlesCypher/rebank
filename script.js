214bb2d0e416c879fdc289fe7d5293ba239b2087
Chiagbaizu Charles <charleschinagorom10.com>
2023-03-15 15:43:09 +0100

    updated

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
 const menu = document.querySelector(".rebank-menu");
-
 const faqs = document.querySelectorAll(".faq");
-
 faqs.forEach((faq) => {
-  const btns = faq.querySelectorAll("svg");
-  btns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      faqs.forEach((question) => {
-        if (question !== faq) {
-          question.classList.remove("active");
-        }
-      });
+  const questions = faq.querySelectorAll(".question");
+  faq.addEventListener("click", () => {
+    questions.forEach((question) => {
+      if (question !== question) faq.classList.remove("active");
       faq.classList.toggle("active");
     });
   });
 });
-
-// faqs.forEach((faq) => {
-//   const questions = faq.querySelectorAll(".question");
-//   faq.addEventListener("click", () => {
-//     questions.forEach((question) => {
-//       if (question !== question) {
-//         faq.classList.remove("active");
-//       }
-//       faq.classList.toggle("active");
-//     });
-//   });
-// });
-
-const year = document.getElementById("year");
-const d = new Date();
-const thisYear = d.getFullYear();
-year.innerText = thisYear + " ";
+const thisYear = new Date().getFullYear();
+const year = (document.getElementById("year").innerText = thisYear);
